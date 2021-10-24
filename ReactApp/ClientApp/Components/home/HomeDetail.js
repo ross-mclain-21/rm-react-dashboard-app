9fee4d751c3148be714cefb382c3a2da68150a93
Ross McLain <ross.mclain21@gmail.com>
2021-10-24 18:36:09 -0400

    move github so it isn't separate import

--- ReactApp/ClientApp/Components/home/HomeDetail.js
+++ ReactApp/ClientApp/Components/home/HomeDetail.js
@@ -1,7 +1,10 @@
 import React from "react";
 import HomeDetailListItem from "./HomeDetailListItem";
-import { faEnvelope, faHashtag } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faHashtag,
+  faGithub,
+} from "@fortawesome/free-solid-svg-icons";
 
 function HomeDetail() {
   return (
