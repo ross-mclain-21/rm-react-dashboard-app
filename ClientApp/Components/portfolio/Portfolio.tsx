f71a37bedd987e9528de1230374b70258d502c75
Ross McLain <ross.mclain21@gmail.com>
2021-11-29 23:26:35 -0500

    remove sticky

--- ClientApp/Components/portfolio/Portfolio.tsx
+++ ClientApp/Components/portfolio/Portfolio.tsx
@@ -84,7 +84,7 @@ function Portfolio() {
             </Button>
           </div>
           <div className="d-flex align-items-start flex-nowrap">
-            <div className="row">
+            <div className="row portfolio-item-list">
               {[...portfolioItemList]
                 .sort(sortPortfolioItems)
                 .map((portfolioItem: IPortfolioItemInput) => (
@@ -100,7 +100,7 @@ function Portfolio() {
                   />
                 ))}
             </div>
-            <div className="ms-sm-4 ms-3 position-sticky top-0">
+            <div className="ms-sm-4 ms-3">
               <p className="fw-bold small text-center">FILTER</p>
               <div className="d-flex flex-column align-items-center justify-content-center portfolio-skill-list">
                 {portfolioSkillList.map(
