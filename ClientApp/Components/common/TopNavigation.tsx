77f9cb768aef1d05c0cf16380b7054991ede31fa
Ross McLain <ross.mclain21@gmail.com>
2021-12-09 00:38:04 -0500

    yee

--- ClientApp/Components/common/TopNavigation.tsx
+++ ClientApp/Components/common/TopNavigation.tsx
@@ -4,20 +4,32 @@ import "../../App.scss";
 import UserAvatar from "./UserAvatar";
 import LogoutButton from "./LogoutButton";
 import LoginButton from "./LoginButton";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function TopNavigation() {
   const { isLoading, isAuthenticated } = useAuth0();
   return (
-    <div className="d-flex align-items-center justify-content-end top-navigation">
-      {process.env.REACT_APP_DEVELOPMENT_ENVIRONMENT != "production" ? (
-        <span className="text-code-red">
-          {process.env.REACT_APP_DEVELOPMENT_ENVIRONMENT}
-        </span>
-      ) : (
-        <></>
-      )}
-      <UserAvatar />
-      {isLoading ? <></> : isAuthenticated ? <LogoutButton /> : <LoginButton />}
-    </div>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      className="bg-transparent py-3"
+    >
+      <Container>
+        <Navbar.Toggle aria-controls="top-navigation" />
+        <Navbar.Collapse id="top-navigation">
+          <div className="d-flex align-items-center justify-content-end flex-fill">
+            <UserAvatar />
+            {isLoading ? (
+              <></>
+            ) : isAuthenticated ? (
+              <LogoutButton />
+            ) : (
+              <LoginButton />
+            )}
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
